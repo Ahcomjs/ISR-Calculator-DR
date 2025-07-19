2e7bedaba2a8b90ec5ed663a0cbea951e448aa6c
Carlos De La Cruz <carlos.delacruz228@gmail.com>
2025-07-19 12:16:44 -0400

    Add img to the app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import CalculationResultsDisplay from './components/CalculationResultsDisplay';
 import AnnualDetailsModal from './components/AnnualDetailsModal';
 import OtherDeductionsModal from './components/OtherDeductionsModal';
 import InfoNote from './components/InfoNote';
+import calculate from './assets/img/calcular-impuestos.png';
 
 const App: React.FC = () => {
   const [grossSalaryInput, setGrossSalaryInput] = useState<string>('');
@@ -23,6 +24,7 @@ const App: React.FC = () => {
     <div className="min-h-screen bg-gradient-to-br from-indigo-700 via-purple-700 to-pink-700 flex items-center justify-center p-2 sm:p-4 font-inter">
       <div className="bg-white rounded-3xl shadow-2xl p-6 md:p-12 w-full max-w-3xl transform transition-all duration-500 hover:scale-[1.01] border-4 border-white border-opacity-20 backdrop-filter backdrop-blur-sm">
         <h1 className="text-3xl sm:text-5xl font-extrabold text-center text-gray-900 mb-10 leading-tight">
+        <img src={calculate} alt="logo" className='mx-auto object-contain w-[110px]' />
           ISR <span className="text-purple-600">Calculator</span> DR
         </h1>
 
